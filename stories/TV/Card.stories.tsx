cb127d13cb822147e4e2e813f96ada4df2deb8bb
leoronne-accedo <120129784+leoronne-accedo@users.noreply.github.com>
2022-12-28 18:33:46 -0300

    feat: adds card component
    adds single card components with landscape and portrait variants

--- stories/TV/Card.stories.tsx
+++ stories/TV/Card.stories.tsx
@@ -5,38 +5,37 @@ import { ThemeProvider } from 'styled-components';
 import { Card, CardProps } from '../../src/components/TV/Card';
 import { theme } from '../../src/styles/theme';
 
-const PlusIcon = (
-  <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24">
-    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
-  </svg>
-);
-
 const meta: Meta = {
   title: 'TV/Card',
   component: Card,
   argTypes: {
-    disabled: {
-      options: [true, false],
-      control: { type: 'inline-radio' },
-    },
     variant: {
-      options: ['default', 'text', 'iconCard'],
-      description: 'Variant type of the Card Component',
+      options: ['portrait', 'landscape'],
+      description: 'Variant type of the component',
       control: { type: 'select' },
     },
-    rightIcon: {
-      options: ['PlusIcon', 'none'],
-      mapping: {
-        PlusIcon: PlusIcon,
-        none: <></>,
-      },
+    size: {
+      options: ['small', 'medium', 'large'],
+      description: 'Size of the component',
+      control: { type: 'select' },
+      defaultValue: 'small',
     },
-    leftIcon: {
-      options: ['PlusIcon', 'none'],
-      mapping: {
-        PlusIcon: PlusIcon,
-        none: <></>,
-      },
+    title: {
+      description: 'Content title',
+      control: { type: 'text' },
+      defaultValue: 'Content Title',
+    },
+    subtitle: {
+      description: 'Content subtitle',
+      control: { type: 'text' },
+    },
+    imgSrc: {
+      description: 'Content image source (URL)',
+      control: { type: 'text' },
+    },
+    progress: {
+      description: 'Progress amount percentage (number between 0-100)',
+      control: { type: 'text' },
     },
   },
   parameters: {
@@ -50,11 +49,25 @@ const meta: Meta = {
 
 export default meta;
 
-const Default = (args: CardProps) => (
+const Portrait = (args: CardProps) => (
   <ThemeProvider theme={theme}>
-    <Card {...args}>
-    </Card>
+    <Card {...args}></Card>
   </ThemeProvider>
 );
 
-export const DefaultCard = Default.bind({});
+export const PortraitCard = Portrait.bind({});
+export const LandScapeCard = Portrait.bind({});
+
+PortraitCard.args = {
+  variant: 'portrait',
+  imgSrc:
+    'https://firebasestorage.googleapis.com/v0/b/accedo-ds.appspot.com/o/black-panther-portrait-2x.png?alt=media',
+};
+
+LandScapeCard.args = {
+  variant: 'landscape',
+  subtitle: 'Secondary text',
+  progress: '43',
+  imgSrc:
+    'https://firebasestorage.googleapis.com/v0/b/accedo-ds.appspot.com/o/black-panther-landscape.png?alt=media',
+};
